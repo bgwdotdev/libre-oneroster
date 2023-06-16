f3d3d44dc436e94f15ab256ade4500114d723d77
bgw <bgw@bgw.dev>
2023-06-17 00:10:03 +0100

    build(nix): add flake package

--- flake.nix
+++ flake.nix
@@ -15,27 +15,44 @@
 
       in
       {
-        devShell = pkgs.mkShell
-          {
-            packages = with pkgs; [
-              cargo
-              rustc
-              rust-analyzer
-              jq.lib
-              openssl.dev
-              sqlite
-              oniguruma
-              pkgconfig
-            ];
-            JQ_LIB_DIR = "${pkgs.jq.lib}";
-            DATABASE_URL = "sqlite:db/oneroster.db";
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            jq.lib
+            openssl.dev
+            sqlite
+            oniguruma
+            pkgconfig
+          ];
+          JQ_LIB_DIR = "${pkgs.jq.lib}";
+          DATABASE_URL = "sqlite:db/oneroster.db";
+        };
 
-          };
-
-        packages = { };
-        #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-        #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "oneroster";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [
+            pkgs.pkgconfig
+            pkgs.openssl.dev
+            pkgs.sqlite
+          ];
+          buildInputs = [
+            pkgs.jq.lib
+            pkgs.oniguruma
+          ];
+          preConfigure = ''
+            sqlite3 db/oneroster.db < db/schema.sql
+            sqlite3 db/oneroster.db < db/init.sql
+          '';
+          doCheck = false;
+          JQ_LIB_DIR = "${pkgs.jq.lib}";
+          DATABASE_URL = "sqlite:db/oneroster.db";
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
       }
     );
 }
